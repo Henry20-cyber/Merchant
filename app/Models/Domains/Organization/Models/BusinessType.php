c956e9bc2d5b61cebbfa0c0781b83c672bd80c2f
Henry20-cyber <onuohahenry1717@gmail.com>
2026-08-06 00:13:29 +0100

    Initial commit

---
+++ app/Models/Domains/Organization/Models/BusinessType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Domains\Organization\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class BusinessType extends Model
+{
+   use HasUuids;
+
+    protected $guarded = [];
+}
---
+++ database/migrations/2026_08_05_224208_create_business_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_types', function (Blueprint $table) {
+
+    $table->uuid('id')->primary();
+
+    $table->string('name', 100)->unique();
+
+    $table->string('icon')->nullable();
+
+    $table->text('description')->nullable();
+
+    $table->boolean('is_active')->default(true);
+
+    $table->timestamps();
+
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_types');
+    }
+};
